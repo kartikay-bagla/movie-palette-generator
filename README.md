50ef6bb2606df23567e79b54286bf3e1441e0460
kartikay-bagla <kartikaybagla@gmail.com>
2023-04-16 05:13:42 +0530

    feat: added command line usage, requirements and improved docs

--- README.md
+++ README.md
@@ -1,6 +1,17 @@
+# Movie Palette Generator
 Generate movie palettes from this script.
 
-Suggestions for video file:
+## Example
+![Movie Palette of Star Wars: Revenge of the Sith](example_outputs/out.png)
+Palette for Star Wars: Revenge of the Sith
+
+## Usage and setup:
+* Create a virtual environment using `python3 -m venv .venv` and activate it.
+* Install the requirements using `pip install -r requirements.txt`.
+* Run `python main.py input.mp4 output.png`.
+* To view other options, use `python main.py --help`.
+
+## Suggestions for video file:
 * Resize to 720p
 * Set it to 1 frame per second i.e. choose one frame for each second. While this may lead to some loss in information, I don't believe its signifcant. Plus you can set it to whatever you want and see the output for yourself.
 
--- main.py
+++ main.py
@@ -1,65 +1,112 @@
 import cv2
 import numpy as np
 
-# Load video file
-video_path = "rots_downsized.mp4"
-cap = cv2.VideoCapture(video_path)
+import typer
+import os
 
-dom_colors = []
 
-# Loop through frames
-while True:
-    # Read frame
-    ret, frame = cap.read()
-    if not ret:
-        break
+def _resize_frame(frame: np.ndarray, factor: int) -> np.ndarray:
+    return cv2.resize(frame, (frame.shape[1] // factor, frame.shape[0] // factor))
 
-    # Resize frame for faster processing (optional)
-    frame = cv2.resize(frame, (frame.shape[1] // 4, frame.shape[0] // 4))
 
+def _get_dom_color_from_frame(frame: np.ndarray):
     # Flatten frame to 1D array
     pixels = frame.reshape(-1, 3)
 
-    # # Calculate dominant color
-    # colors, counts = np.unique(pixels, axis=0, return_counts=True)
-    # dominant_color = colors[np.argmax(counts)]
-    # print(dominant_color)
-
     k = 1  # number of clusters
     criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
-    _, labels, centers = cv2.kmeans(np.float32(pixels), k, None, criteria, 10, cv2.KMEANS_RANDOM_CENTERS)
+    _, _, centers = cv2.kmeans(
+        np.float32(pixels), k, None, criteria, 10, cv2.KMEANS_RANDOM_CENTERS
+    )
 
     # Convert center color from float to integer
-    dominant_color = np.round(centers[0]).astype(int)
-    dom_colors.append(dominant_color)
+    return np.round(centers[0]).astype(int)
 
-    # Display dominant color
-    # dominant_color_display = np.zeros((100, 100, 3), dtype=np.uint8)
-    # dominant_color_display[:, :] = dominant_color
-    # cv2.imshow("Dominant Color", dominant_color_display)
-    # cv2.imshow("Frame", frame)
 
-    # key = cv2.waitKey(1) & 0xFF
-    # if key == ord('q'):  # Quit
-    #     break
-    # elif key == ord('n'):  # Next frame
-    #     continue
+def _display_dom_color_and_frame(frame: np.ndarray, dominant_color: np.ndarray):
+    dominant_color_display = np.zeros((100, 100, 3), dtype=np.uint8)
+    dominant_color_display[:, :] = dominant_color
+    cv2.imshow("Dominant Color", dominant_color_display)
+    cv2.imshow("Frame", frame)
 
-out_array = np.zeros((500, len(dom_colors), 3), dtype=np.uint8)
-for i, col in enumerate(dom_colors):
-    out_array[:, i, :] = col
 
-cv2.imshow("test", out_array)
+def _create_output_frame(
+    dom_colors: list[np.ndarray], output_height: int, output_length: int
+) -> np.ndarray:
+    out_array = np.zeros((output_height, len(dom_colors), 3), dtype=np.uint8)
+    for i, col in enumerate(dom_colors):
+        out_array[:, i, :] = col
+    return cv2.resize(out_array, (output_length, output_height))
 
-resized = cv2.resize(out_array, (1920, 540))
-cv2.imshow("resized", resized)
 
-cv2.imwrite("rots_output.png", resized)
+def create_palette(
+    input_file_name: str = typer.Argument(
+        ..., help="Path to input file. Example: input.mp4"
+    ),
+    output_file_name: str = typer.Argument(
+        ...,
+        help="Path to output file. Will raise error if file already exists unless"
+        " output_file_overwrite is set. Example: output.png",
+    ),
+    output_file_overwrite: bool = typer.Option(
+        False, help="If this is set, will overwrite output file if it exists."
+    ),
+    input_resize_factor: int = typer.Option(
+        1, help="Factor by which to scale down image. Cannot be less than 1."
+    ),
+    output_height: int = typer.Option(540, help="Height of output palette in pixels."),
+    output_length: int = typer.Option(1920, help="Length of output palette in pixels."),
+    debug_show_dom_color_and_frame: bool = typer.Option(
+        False,
+        help="Show dominant color for each frame. Press q to quit, n to move forward.",
+    ),
+):
+    # Load video file
+    if not os.path.exists(input_file_name):
+        raise FileNotFoundError("Given input file does not exist.")
+    cap = cv2.VideoCapture(input_file_name)
 
-key = cv2.waitKey(0) & 0xFF
-if key == ord('q'):  # Quit
-    # break
+    dom_colors = []
 
-    # Release video capture and close all windows
+    # Loop through frames
+    while True:
+        # Read frame
+        ret, frame = cap.read()
+        if not ret:
+            break
+
+        # Resize frame for faster processing (optional)
+        if input_resize_factor >= 1:
+            frame = _resize_frame(frame=frame, factor=input_resize_factor)
+
+        # Get dominant color
+        dominant_color = _get_dom_color_from_frame(frame=frame)
+        dom_colors.append(dominant_color)
+
+        # Display dominant color
+        if debug_show_dom_color_and_frame:
+            _display_dom_color_and_frame(frame=frame, dominant_color=dominant_color)
+            key = cv2.waitKey(1) & 0xFF
+            if key == ord("q"):  # Quit
+                break
+            elif key == ord("n"):  # Next frame
+                continue
+
+    output_frame = _create_output_frame(
+        dom_colors=dom_colors, output_height=output_height, output_length=output_length
+    )
+
+    if os.path.exists(output_file_name) and not output_file_overwrite:
+        raise FileExistsError(
+            "Given output file already exists. To overwrite, use output_file_overwrite."
+        )
+    cv2.imwrite(output_file_name, output_frame)
+
     cap.release()
-    cv2.destroyAllWindows()
+
+    if debug_show_dom_color_and_frame:
+        cv2.destroyAllWindows()
+
+
+if __name__ == "__main__":
+    typer.run(create_palette)
